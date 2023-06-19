f5bedaf47bdb42d71d314653dbc36aefdb93a196
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2023-06-18 22:33:52 -0300

    corregido tp3 para cumplir con los requisitos de la consigna

--- practico-03-transformacion-perspectiva/perspectiva-v3.py
+++ practico-03-transformacion-perspectiva/perspectiva-v3.py
@@ -3,35 +3,64 @@ import numpy as np
 
 
 fondo = cv2.imread('hoja.png',cv2.IMREAD_GRAYSCALE)
-testImg = cv2.imread('hse1.jpg',cv2.IMREAD_GRAYSCALE)
+testImg = cv2.imread('perspective.jpg',cv2.IMREAD_GRAYSCALE)
+mask = np.zeros((fondo.shape[1],fondo.shape[0]),np.uint8)
+
+def computeL2Media(ref_point):
+    # funcion que recibe las coordenadas seleccionadas en la función original,
+    # determina el largo y ancho del paralelogramo que resulta de ellas para luego determinar la relación de aspecto
+    width_AD = np.sqrt(((ref_point[0][0]-ref_point[3][0])**2)+((ref_point[0][1]-ref_point[3][1])**2))
+    width_BC = np.sqrt(((ref_point[1][0]-ref_point[2][0])**2)+((ref_point[1][1]-ref_point[2][1])**2))
+    maxWidth = max(int(width_AD), int(width_BC))
+
+    height_AB = np.sqrt(((ref_point[0][0]-ref_point[1][0])**2)+((ref_point[0][1]-ref_point[1][1])**2))
+    height_CD = np.sqrt(((ref_point[2][0]-ref_point[3][0])**2)+((ref_point[2][1]-ref_point[3][1])**2))
+    maxHeight = max(int(height_AB), int(height_CD))
+
+    dimensions = (maxWidth, maxHeight)
+    return dimensions
+
+
 
 
 ref_point = []
-mask = None
+result = None
 
 def draw_dots(event,x,y,flags,param):
     global ref_point,fondo,testImg,mask,result
+   
+    trImg_w_borders = np.zeros((fondo.shape[1],fondo.shape[0]),np.uint8)
+
 
     if event == cv2.EVENT_LBUTTONDOWN:
         if len(ref_point)<4:
             ref_point.append([x,y])
-            cv2.circle(fondo,ref_point[-1],3,(0,0,255),-1)
-            cv2.imshow("image",fondo)
+            cv2.circle(testImg,ref_point[-1],3,(0,0,255),-1)
+            cv2.imshow("image",testImg)
         if len(ref_point) == 4:
-            scrCoordinates = np.array([[0,0],[testImg.shape[1],0],[testImg.shape[1],testImg.shape[0]],[0,testImg.shape[0]]]).astype(np.float32)
+            maxWidth, maxHeight = computeL2Media(ref_point)
+            print(ref_point)
+            
+            srcCoordinates = np.array(ref_point).astype(np.float32)
+            dstCoordinates = np.array([[0, 0], [0, maxHeight - 1], [maxWidth - 1, maxHeight - 1], [maxWidth - 1, 0]]).astype(np.float32)
 
-            dstCoordinates = np.array([ref_point[0],ref_point[1],ref_point[2],ref_point[3]]).astype(np.float32)
-            M = cv2.getPerspectiveTransform(scrCoordinates,dstCoordinates)
-            trImg = cv2.warpPerspective(testImg,M,(fondo.shape[1],fondo.shape[0]))
-            mask = (trImg > 0) * 1 
-            result = (1 - mask) * fondo + (mask) * trImg
+            M = cv2.getPerspectiveTransform(srcCoordinates,dstCoordinates)
+            trImg = cv2.warpPerspective(testImg,M,(maxWidth,maxHeight),flags=cv2.INTER_LINEAR)
+
+            y_offset = (fondo.shape[1] - maxHeight) // 2
+            x_offset = (fondo.shape[0] - maxWidth ) // 2    
+
+            trImg_w_borders[y_offset:y_offset+maxHeight, x_offset:x_offset+maxWidth] = trImg
+
+            mask = (trImg_w_borders > 0) * 1 
+            result = (1 - mask) * fondo + (mask) * trImg_w_borders
 
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_dots)
 
 while True:
-    cv2.imshow('image',fondo)
-    if mask is not None:
+    cv2.imshow('image',testImg)
+    if result is not None:
         result = cv2.convertScaleAbs(result)
         cv2.imshow('output',result)
 
